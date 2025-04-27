e0ec90e30dd333316ae8bc59c57c7ed88c913486
arsany shafik <arsany.shafik.fci.scu@gmail.com>
2025-04-27 19:46:28 +0300

    Updates

--- api.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const res = axios
-  .post('https://car-mate-t012.onrender.com/api/v1/users/login', {
-    email: 'abc1@gmail.com',
-    password: 'password',
-  })
-  .then((response) => console.log(response.data))
-  .catch((error) => console.log(error));
